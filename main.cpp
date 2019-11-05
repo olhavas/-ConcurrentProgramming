dd2d3707160554f9935aefee6ed7436869c4f82c
Olha Vasylyshyn <Olha.Vasylyshyn@harman.com>
2019-11-05 17:02:01 +0100

    wczytywanie

--- main.cpp
+++ main.cpp
@@ -3,74 +3,50 @@
 #include <pthread.h>
 #include <thread>
 #include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
 
 using namespace std;
-std::mutex m_mutex;
 
-void shared_print(std::string str)
+
+void rand_and_save()
 {
-
-    std::lock_guard<std::mutex> locker(m_mutex);
-    std::thread::id this_id = std::this_thread::get_id();
-    cout<<this_id<<' '<<str<<'\n';
-}
-
-void fibonacci(const int num)
-{
-    std::string fib = "";
-    if(num == 0) {
-        fib.append(to_string(num));
-        shared_print(fib);
-        fib.append(" ");
-    }
-    else if(num >= 2)
+    ofstream myfile;
+    myfile.open ("../inputdata.txt");
+    for (int i = 0; i <10000000; i++)
     {
-        int a = 0, b = 1;
-        fib.append(std::to_string(a)+ " ");
-        fib.append(std::to_string(b)+ " ");
-        for(int i=0; i < (num-1); i++)
-        {
-            std::swap(a, b);
-            b += a;
-            fib.append(std::to_string(b)+ " ");
-            shared_print(fib);
-        }
+        myfile << static_cast<int>(rand() % 256) << " ";
     }
-
+    myfile.close();
 }
-void fib_sum(const int num)
-{
-    std::string fib = "";
+void read_from_file()
 
-    if(num == 0) {
-        fib.append(0);
-        shared_print(fib);
-        fib.append(" ");
-    }
-    else if(num>= 2)
-    {
-        int a = 0, b = 1, sum = 0;
-        for(int i=0; i < (num-1); i++)
-        {
-            std::swap(a, b);
-            b += a;
-            sum+= b;
-            fib.append(std::to_string(sum)+ " ");
-            shared_print(fib);
-
-        }
-    }
-}
 
 int main() {
-    int n = 10;
-    std::thread first(fibonacci, n);
-    std::thread second(fib_sum, n);
-    cout<< "main \n";
-    if(first.joinable())
-    first.join();
-    if(second.joinable())
-    second.join();
+
+    srand (time(NULL));
+    //generate file with input data
+    //rand_and_save();
+    uint num_of_threads = 1;
+    cout<< "Change quantity of thread (2, 4, 8, 10, 12, 14, 16): ";
+    cin>>num_of_threads;
+    vector 
+
+
+    if(num_of_threads == 0 || num_of_threads ==1)
+    {
+
+    }
+
+
+
+
+
+
+
 
 
 
