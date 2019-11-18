72f2327f32c6c750d5ad8741de4d1e14c5d6846d
Olga Vasylyshyn <olickav@gmail.com>
2019-11-18 07:20:35 +0100

    needs fix

--- main.cpp
+++ main.cpp
@@ -38,6 +38,17 @@ void read_from_file( std::vector<int> &data,  std::string nameoffile)
 
 }
 
+void writeToFile(std::array<int, 256> data, std::string nameoffile)
+{
+    std::ofstream  out;
+    out.open("../"+nameoffile);
+    for(auto i =0; i < data.size(); i++)
+    {
+        out<<i<<'='<<data[i]<<'\n';
+    }
+    out.close();
+}
+
 std::vector<int> readFromFile(std::string nameOfFile) {
     std::ifstream in("../"+nameOfFile, std::ios::in);
     std::vector<int> result;
@@ -68,42 +79,51 @@ int main(int argc, const char * argv[]) {
 
 
     int dataSize = data.size();
-    Result result;
-
     int numberOfThreads = 1;
-    int blockSize = dataSize / numberOfThreads;
-    int lastThreadSize = (dataSize - blockSize * numberOfThreads) + blockSize;
 
-    std::vector<std::thread> threadVector;
+   // while (numberOfThreads > 0) {
+        Result *result = new Result();
+        numberOfThreads = 1;
 
-    for (int i = 0 ; i<numberOfThreads - 1; i++){
-        threadVector.push_back(std::thread([&](){
-            int first = i * blockSize;
-            int last = (i + 1) * blockSize - 1;
-            for (int j = first; j<=last; j++) {
-                result.incrementOnIndex(data[j]);
+        std::cout << "num of thread <2, 4, 8,10 , 12 , 14 , 16>";
+        std::cin >> numberOfThreads;
+        if (numberOfThreads == 1) {
+            for (int j = 0; j <= dataSize; j++) {
+                result->incrementOnIndex(data[j]);
             }
-        }));
-    }
-    threadVector.push_back(std::thread([&](){
-        int first = (numberOfThreads - 1) * blockSize;
-        int last = ((numberOfThreads - 1) * blockSize - 1) + lastThreadSize;
-        for (int j = first; j<=last; j++) {
-            result.incrementOnIndex(data[j]);
+        } else {
+
+
+            int blockSize = dataSize / numberOfThreads;
+            int lastThreadSize = (dataSize - blockSize * numberOfThreads) + blockSize;
+
+            std::vector<std::thread> threadVector;
+
+            for (int i = 0; i < numberOfThreads - 1; i++) {
+                threadVector.push_back(std::thread([&]() {
+                    int first = i * blockSize;
+                    int last = (i + 1) * blockSize - 1;
+                    for (int j = first; j <= last; j++) {
+                        result->incrementOnIndex(data[j]);
+                    }
+                }));
+            }
+            threadVector.push_back(std::thread([&]() {
+                int first = (numberOfThreads - 1) * blockSize;
+                int last = ((numberOfThreads - 1) * blockSize - 1) + lastThreadSize;
+                for (int j = first; j <= last; j++) {
+                    result->incrementOnIndex(data[j]);
+                }
+            }));
+
+            for (auto &thread : threadVector) {
+                if (thread.joinable()) thread.join();
+            }
         }
-    }));
 
-    for (auto& thread : threadVector) {
-        if(thread.joinable()) thread.join();
-    }
-
-
-    std::cout << result.histogram[0];
-    auto suma = 0;
-    for(auto x : result.histogram) {
-        suma+=x;
-    }
-    std::cout << suma;
+        writeToFile(result->histogram, "out" + std::to_string(numberOfThreads) + ".txt");
+        delete result;
+   // }
 
     return 0;
 }
